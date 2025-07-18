52245c293cfc3bd91aa424b6a8471a43b973725f
dummy <dest24199@gmail.com>
2025-07-18 20:17:30 +0200

    Create init.lua

---
+++ UserInput/ClientModulesUtils/init.lua
@@ -0,0 +1,9 @@
+local _Input = require(script._Input)
+export type GamepadType = _Input.GamepadType
+export type MouseType = _Input.MouseType
+export type KeyboardType = _Input.KeyboardType
+export type TouchType = _Input.TouchType
+
+return {
+  Input = _Input
+}
